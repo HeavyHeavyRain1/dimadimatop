c40086926ea3743961cc6039fc687d4fbfe04a97
Dmitriy <dimagorikheavyrain@yandex.by>
2024-11-22 18:35:38 +0300

    Сделал ещё раз

---
+++ Papka_moya/testi_chisla.py
@@ -0,0 +1,12 @@
+from Calcul import Calculator
+
+# Задание
+# 1. Первому челу чё-то
+# 2. Второму тоже
+# 3/ sdfsfs
+# 4. Работай животное
+
+calc = Calculator()
+calc.sum(10, 6)
+calc.print_last_res()
+
